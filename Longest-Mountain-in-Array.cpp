79b9a489cae59e172c85de26f1eed994563a9a48
Saloni Garg <gargsaloni247@gmail.com>
2026-01-20 17:49:33 +0530

    [845] [Time Beats: 100.00%] [Memory Beats: 80.47%] - LeetPush

--- Longest-Mountain-in-Array.cpp
+++ Longest-Mountain-in-Array.cpp
@@ -1,25 +1,23 @@
 1class Solution {
 2public:
 3    int longestMountain(vector<int>& arr) {
-4        int n=arr.size();
-5        if(n<3) return 0;
-6        vector<int> up(n,0), down(n,0);
-7        for(int i=1;i<n;i++){
-8            if(arr[i] > arr[i-1]){
-9                up[i]=up[i-1]+1;
-10            }
-11        }
-12        for(int i=n-2;i>=0;i--){
-13            if(arr[i] > arr[i+1]){
-14                down[i]=down[i+1]+1;
-15            }
-16        }
-17        int longest=0;
-18        for(int i=1;i<n-1;i++){
-19            if(up[i]>0 && down[i]>0){
-20                longest=max(longest,up[i]+down[i]+1);
-21            }
-22        }
-23        return longest;
-24    }
-25};
+4     int n=arr.size(), length=0;
+5     int i=1;
+6     for(i=1;i<n-1;i++){
+7        if(arr[i]>arr[i-1] && arr[i]>arr[i+1]){
+8          int  left=i;
+9             int right=i;
+10            while(left>0 && arr[left]>arr[left-1]){
+11                left--;
+12            }
+13            while(right<n-1 && arr[right+1]<arr[right]){
+14                right++;
+15               
+16            }
+17            length=max(length,right-left+1);
+18        }
+19     }
+20        return length;
+21    }
+22    
+23};
