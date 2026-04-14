4dae2002fc05c089789335612c358e8653680c4f
Saloni Garg <gargsaloni247@gmail.com>
2026-04-14 19:35:15 +0530

    Time: 3 ms (34.89%) | Memory: 16.2 MB (34.56%) - LeetSync

---
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n=prices.size();
+        vector<vector<int>> dp(n+2,vector<int>(2,0));
+        for(int idx=n-1;idx>=0;idx--){
+            for(int buy=0;buy<=1;buy++){
+               if(buy==1){
+                dp[idx][buy]=max(-prices[idx]+dp[idx+1][0],0+dp[idx+1][1]);
+               }else{
+                dp[idx][buy]=max(prices[idx]+dp[idx+2][1],0+dp[idx+1][0]);
+               }
+            }
+        }
+        return dp[0][1];
+    }
+};
