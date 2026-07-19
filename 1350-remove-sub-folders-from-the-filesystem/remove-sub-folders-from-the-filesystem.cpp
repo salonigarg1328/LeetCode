10c7707b5c257b4dfad51a1aaa22e7b323483e16
Saloni Garg <gargsaloni247@gmail.com>
2026-07-19 19:36:52 +0530

    Time: 68 ms (46.07%) | Memory: 55.4 MB (43.39%) - LeetSync

---
+++ 1350-remove-sub-folders-from-the-filesystem/remove-sub-folders-from-the-filesystem.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<string> removeSubfolders(vector<string>& folder) {
+        sort(begin(folder),end(folder));
+        vector<string> result;
+        result.push_back(folder[0]);
+        for(int i=1;i<folder.size();i++){
+            string currfolder=folder[i];
+            string lastfolder=result.back();
+            lastfolder += '/';
+            if(currfolder.find(lastfolder)!=0){
+                result.push_back(currfolder);
+            }
+        }
+        return result;
+    }
+};
